b1be35a486ff1493a2c768f977f4f1c06e5eb5ae
Allan Nielsen <allannielsen@Allans-MacBook.local>
2015-09-15 15:33:50 -0500

    units and summary fields added

--- models/activities.js
+++ models/activities.js
@@ -5,9 +5,11 @@ var ActivitiesSchema = new Schema({
     user_id: {type: String, required: true},
     duration: Number,
     distance: Number,
+    units: String,
     effort: String,
     activity_type: String,
-    date: Date
+    date: Date,
+    summary: String
 });
 
 module.exports = mongoose.model("activities", ActivitiesSchema);

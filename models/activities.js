0d079de844cfe1ab2ca7f79f9186a1af1e47785d
Allan Nielsen <allannielsen@Allans-MacBook.local>
2015-09-15 15:09:26 -0500

    user_id added, typo corrected

--- models/activities.js
+++ models/activities.js
@@ -1,3 +1,13 @@
-/**
- * Created by allannielsen on 15/09/15.
- */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema();
+
+var ActivitiesSchema = new Schema({
+    user_id: String,
+    duration: Number,
+    distance: Number,
+    effort: String,
+    activity_type: String,
+    date: Date
+});
+
+module.exports = mongoose.model("activities", ActivitiesSchema);

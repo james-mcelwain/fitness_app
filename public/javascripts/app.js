e75b8e61fbce9026944b2817c21ae38c89af6bb8
James McElwain <jmcelwain@gmail.com>
2015-09-15 17:24:45 -0500

    Kate's stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,33 @@
-var app = angular.module('fitnessApp');
+var app = angular.module('fitnessApp', []);
+​
+​
+//get stats
+app.service('Stats', function ($http) {
+    var urlBase = '/statistics/userstats';
+    this.getStats = function(){
+        return $http({
+            method: 'GET',
+            url: urlBase + user.id
+        });
+    }
+});
+​
+app.controller('myCtrl', function($scope, Stats) {
+    Stats.getStats().then(function(response){
+        $scope.user = response.data;
+    });
+});
+​
+//app.controller(/*'CONTROLLER'*/, [$scope], [$http], function($scope, $http){
+//    $scope./*function*/ = function(user){
+//        $http({
+//            method: 'GET',
+//            url: '/statistics/userstats' + user.id,
+//            data: user,
+//            datatype: JSON,
+//        }.then(function(response){
+//                $scope./*stat*/ = response.data;
+//            });
+//    });
+//});
+​

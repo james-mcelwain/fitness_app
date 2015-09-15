439f396c5bffb6f2935f9cf06c60fc33086b512f
Jason Spidle <jason.spidle@gmail.com>
2015-09-15 15:06:21 -0500

    Create shell route for comments

---
+++ routes/comments.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET route */
+// get comments for activity
+
+router.get('/', function(req, res, next) {
+
+});
+
+/* POST route */
+// post comment to activity
+
+router.post('/', function(req, res, next) {
+
+});
+
+/* DELETE route */
+
+router.delete('/', function(req, res, next) {
+
+    // delete comment from activity by activity user
+
+    // delete comment from activity by comment user
+
+});
+
+module.exports = router;

5966559f9a44693cc7321b94b24132e11f916dea
Jason Spidle <jason.spidle@gmail.com>
2015-09-15 15:31:27 -0500

    Add moment.js

--- routes/comments.js
+++ routes/comments.js
@@ -3,25 +3,28 @@ var router = express.Router();
 
 /* GET route */
 // get comments for activity
-
 router.get('/', function(req, res, next) {
-
+    res.json('Comments')
 });
 
 /* POST route */
 // post comment to activity
-
 router.post('/', function(req, res, next) {
-
+    res.json('Comment posted')
 });
 
 /* DELETE route */
-
 router.delete('/', function(req, res, next) {
 
     // delete comment from activity by activity user
+    if (user){
+        res.json('Comment deleted by user')
+    }
 
     // delete comment from activity by comment user
+    if (commenter){
+        res.json('Comment deleted by commenter')
+    }
 
 });
 

e50aa36a962bbdfb209a85e30c60204479f70f52
Antoinette Smith <antoinette@primeacademy.io>
2015-09-16 11:27:14 -0500

    Someone left merge code in activities

--- models/activities.js
+++ models/activities.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
-
-var ActivitiesSchema = new Schema({
-    user_id: {type: String},
-    duration: {type: Number},
-    distance: {type: Number},
-    effort: {type: String},
-    activity_type: {type: String},
-    date: {type: Date}
-=======
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
 var ActivitiesSchema = new Schema({
     user_id: {type: String, required: true},
@@ -22,7 +11,6 @@ var ActivitiesSchema = new Schema({
     activity: String,
     date: Date,
     summary: String
->>>>>>> 235434a3e9077e4fbb42ab0c6e5bc58a72c77f28
 });
 
 module.exports = mongoose.model("activities", ActivitiesSchema);
